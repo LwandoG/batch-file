cd741f2d6850adc19cdcf862cc3989d2af92f5d1
avinash <avinashadams001@gmail.com>
2020-02-27 16:12:48 +0200

    second commit

---
+++ movies8.bat
@@ -0,0 +1,19 @@
+cd/
+cd C:\Users\user\desktop
+md movies
+cd movies
+md 2018 2019 2020
+cd 2018
+type nul > movie1.txt
+type nul > movie2.txt
+type nul > movie3.txt
+cd..
+cd 2019
+type nul > movie4.txt
+type nul > movie5.txt
+type nul > movie6.txt
+cd..
+cd 2020
+type nul > movie7.txt
+type nul > movie8.txt 
+type nul > movie9.txt
